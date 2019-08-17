5cadb838951ac7c742807a231935d98ab51abe1f
Paulo Cesar Loayza <pcloayza@gmail.com>
2019-08-16 20:53:48 -0400

    Primer Commit

---
+++ webAPIstudent/webAPIstudent/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace webAPIstudent.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ webAPIstudent/webAPIstudent/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace webAPIstudent.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
